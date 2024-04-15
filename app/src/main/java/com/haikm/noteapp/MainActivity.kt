5ded28a63aee58f3cbbabf8ed07838f70e16ef2a
HaiKM <haikm@apero.vn>
2024-04-15 20:32:46 +0700

    Initial commit

---
+++ app/src/main/java/com/haikm/noteapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.haikm.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.haikm.noteapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
